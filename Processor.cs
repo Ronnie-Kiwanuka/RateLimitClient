2dca7a1c4f39eea602a4489c410af8bbe0722f2c
Ronnie Kiwanuka <Ronnie Kiwanuka@RONNIE-KIWANUKA>
2022-09-21 11:39:34 +0300

    Add project files.

---
+++ Processor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingClient
+{
+    public class Processor
+    {
+        string url = "https://localhost:7259/weatherforecast";
+        string jsonRequest = "";
+
+
+        public string CallApi()
+        {
+            string result = "";
+            string data = "";
+            try
+            {
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                //request.ContentLength = data.Length;
+                request.Headers.Add("Client-Id", "test123");
+
+                var response = (HttpWebResponse)request.GetResponse();
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                result = responseString;
+            }
+            catch (Exception ee)
+            {
+                result = ee.Message;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Processor proc = new Processor();
+            string response = proc.CallApi();
+
+            Console.WriteLine("Response: " + response);
+        }
+    }
+}
